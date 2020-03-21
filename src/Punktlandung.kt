e7601c54ffd10631c67e285b0de8f0bb0304c352
Rasmus Kuschel <rasmushofmann@gmail.com>
2020-03-21 09:33:01 +0100

    solution for 1

--- src/Punktlandung.kt
+++ src/Punktlandung.kt
@@ -1,11 +1,22 @@
 object Punktlandung {
 
+    /**
+    1:
+    (0, 3)
+    (2, 4)
+    (2, 6)
+    (1, 7)
+    (5, 6)
+    (1, 7)
+    (3, 5)
+    (0, 4)
+     */
+
     @JvmStatic
     fun main(args: Array<String>) {
         println("Punktlandung")
 
         println(enumerate2of8.size)
-        // enumerate2of8.forEach { println(it) }
 
         var count = 0;
         for (row1 in enumerate2of8) {
@@ -20,6 +31,15 @@ object Punktlandung {
                                             for (row8 in legal(row1, row2, row3, row4, row5, row6, row7)) {
                                                 if (row8.first == 4 || row8.second == 4 ) {
                                                     count++
+
+                                                    println(row1)
+                                                    println(row2)
+                                                    println(row3)
+                                                    println(row4)
+                                                    println(row5)
+                                                    println(row6)
+                                                    println(row7)
+                                                    println(row8)
                                                 }
                                             }
                                         }
@@ -31,6 +51,7 @@ object Punktlandung {
                 }
             }
         }
+
         println(count)
     }
 
@@ -44,33 +65,65 @@ object Punktlandung {
         result.toList()
     }
 
-    private fun checkDiagonals(givenRows: Array<Pair<Int, Int>>, candidate: Pair<Int, Int>, rowIndex0: Int, rowIndex1: Int): Boolean {
-        val row1 = givenRows[rowIndex0]
-        val row2 = givenRows[rowIndex1]
-        val index = givenRows.size
+    private fun pointsOnLine(point1: Pair<Int, Int>, point2: Pair<Int, Int>, point3: Pair<Int, Int>): Boolean {
+        val diffH1 = point2.second - point1.second
+        val diffH2 = point3.second - point2.second
 
-        val forbidden1 = (row2.first - row1.first) * (index - 1) + row2.first
-        val forbidden2 = (row2.second - row1.first) * (index - 1) + row2.second
-        val forbidden3 = (row2.first - row1.second) * (index - 1) + row2.first
-        val forbidden4 = (row2.second - row1.second) * (index - 1) + row2.second
+        val diffV1 = point2.first - point1.first
+        val diffV2 = point3.first - point2.first
 
-        return candidate.first != forbidden1 && candidate.first != forbidden2 && candidate.first != forbidden3 && candidate.first != forbidden4 &&
-            candidate.second != forbidden1 && candidate.second != forbidden2 && candidate.second != forbidden3 && candidate.second != forbidden4
+        val prop1: Double = diffH1.toDouble() / diffV1.toDouble()
+        val prop2: Double = diffH2.toDouble() / diffV2.toDouble()
+
+        return prop1 == prop2
+    }
+
+    private fun checkDiagonalSingle(givenRows: Array<Pair<Int, Int>>, candidate: Pair<Int, Int>, rowIndex0: Int, rowIndex1: Int): Boolean {
+        val candidateIndex = givenRows.size
+
+        val row0 = givenRows[rowIndex0]
+        val row1 = givenRows[rowIndex1]
+
+        val candidatePoint1 = Pair(candidate.first, candidateIndex)
+        val candidatePoint2 = Pair(candidate.second, candidateIndex)
+
+        val point01 = Pair(row0.first, rowIndex0)
+        val point02 = Pair(row0.second, rowIndex0)
+
+        val point11 = Pair(row1.first, rowIndex1)
+        val point12 = Pair(row1.second, rowIndex1)
+
+        return !pointsOnLine(point01, point11, candidatePoint1) &&
+                !pointsOnLine(point02, point11, candidatePoint1) &&
+                !pointsOnLine(point01, point12, candidatePoint1) &&
+                !pointsOnLine(point02, point12, candidatePoint1) &&
+                !pointsOnLine(point01, point11, candidatePoint2) &&
+                !pointsOnLine(point02, point11, candidatePoint2) &&
+                !pointsOnLine(point01, point12, candidatePoint2) &&
+                !pointsOnLine(point02, point12, candidatePoint2)
+    }
+
+    private fun checkDiagonals(givenRows: Array<Pair<Int, Int>>, candidate: Pair<Int, Int>): Boolean {
+        for (index0 in 0 .. givenRows.size-1) {
+            for (index1 in index0+1 .. givenRows.size-1) {
+                if (!checkDiagonalSingle(givenRows, candidate, index0, index1)) {
+                    return false
+                }
+            }
+        }
+        return true
     }
 
     // 1. only 2 in column        : 3508230
     // 2. diagonals of rows 1 % 2 : 1598002
-    // 3. diagonals of all rows   :
+    // 3. diagonals of all rows   : 1
+
     fun legal(vararg givenRows: Pair<Int, Int>): List<Pair<Int, Int>> {
         return enumerate2of8.filter {
             val atPos0: Int = givenRows.count { row -> row.first == it.first || row.second == it.first }
             val atPos1: Int = givenRows.count { row -> row.second == it.second || row.first == it.second }
 
-            if (atPos0 < 2 && atPos1 < 2) {
-                checkDiagonals(givenRows as Array<Pair<Int, Int>>, it, 0, 1)
-            } else {
-                false
-            }
+            atPos0 < 2 && atPos1 < 2 && checkDiagonals(givenRows as Array<Pair<Int, Int>>, it)
         }
     }
 }
